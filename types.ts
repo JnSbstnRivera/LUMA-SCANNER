2be6f07e1ea3882cc73e9cf35b8370b9055722fb
JnSbstnRivera <juan.s@windmarhome.com>
2026-04-10 10:19:59 -0500

    feat: agregar powerwallCount al tipo SolarSystemRecommendation

--- types.ts
+++ types.ts
@@ -25,6 +25,7 @@ export interface SolarSystemRecommendation {
   estimatedMonthlyGeneration: number;
   offsetPercentage: number;
   batteryRecommendationKwh: number;
+  powerwallCount: number;
 }
 
 export interface AnalysisResult {
